4cd740eb13608e5ebd715d001aa24fd1b47aa907
aleksandr <antarctida.inc@gmail.com>
2020-09-05 01:56:08 +0300

    Templates header

--- header.php
+++ header.php
@@ -2,15 +2,13 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Library</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbag-light bg-light">
-    <a href="?pages=index" class="nav-link">Home</a>
-    <a href="?pages=addbook" class="nav-link">Добавить книгу</a>
-    <a href="?pages=book" class="nav-link">Рейтинг авторов</a>
-    <a href="?pages=update" class="nav-link text-right">Обновление записей</a>
-    <!--        <a href="" class="nav-link">Sing in</a>-->
+<nav>
+    <a href="./">Home</a>
+    <a href="?pages=add_book">Добавить книгу</a>
+    <a href="?pages=book">Рейтинг авторов</a>
+    <a href="?pages=update">Обновление записей</a>
 </nav>
 <br>
