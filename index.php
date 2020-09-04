dfd54ff28717525b6cb38c786d139bbee086c1df
aleksandr <antarctida.inc@gmail.com>
2020-09-05 01:51:37 +0300

    Fixed routing.

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
-require('./header.php');
-
-if (isset($_GET['pages'])) {
+require 'header.php';
+if (isset($_GET['pages'])){
     $page = htmlentities($_GET['pages']);
-    $path = './pages/' . $page . '.php';
-    if (file_exists($path)) {
+    $path = './pages/'.$page.'.php';
+    if (file_exists($path)){
         include($path);
+        die();
     }
-} else {
-    include('./index.php');
+}else{
+    include('./pages/home.php');
 }
+
+
+require 'footer.php';
