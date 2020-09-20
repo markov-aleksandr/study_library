94d5f3f4af13e333ae4407fb0a7eeb73194ae41e
aleksandr <antarctida.inc@gmail.com>
2020-09-20 22:31:00 +0300

    start application

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
-require 'header.php';
-if (isset($_GET['pages'])){
-    $page = htmlentities($_GET['pages']);
-    $path = './pages/'.$page.'.php';
-    if (file_exists($path)){
-        include($path);
-        die();
-    }
-}else{
-    include('./pages/home.php');
-}
 
+use Application\Core\Route;
 
-require 'footer.php';
+require_once 'vendor/autoload.php';
+
+Route::start();
