5330a028a19175e54ca93c94bcab0a883352e2a2
aleksandr <gerka2281337@gmail.com>
2020-10-27 20:08:15 +0200

    test

--- application/core/Route.php
+++ application/core/Route.php
@@ -13,11 +13,11 @@ class Route
         $actionName = 'index';
         $routes = explode('/', $_SERVER['REQUEST_URI']);
 
-        if (!empty($routes[2])) {
-            $controllerName = $routes[2];
+        if (!empty($routes[1])) {
+            $controllerName = $routes[1];
         }
-        if (!empty($routes[3])) {
-            $actionName = $routes[3];
+        if (!empty($routes[2])) {
+            $actionName = $routes[2];
         }
 
         $modelName = ucfirst($controllerName) . 'Model';
@@ -51,9 +51,6 @@ class Route
 
     public static function ErrorPage404()
     {
-        $host = 'http://' . $_SERVER['HTTP_HOST'] . '/library/';
-        header('HTTP/1.1 404 Not Found');
-        header("Status: 404 Not Found");
-        header('Location:' . $host . 'error404');
+        echo  '1';
     }
 }
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-
+var_dump($_SERVER['REQUEST_URI']);
+ini_set('display_errors', 1);
 use Application\Core\Route;
 
 require_once 'vendor/autoload.php';
 
 Route::start();
+
